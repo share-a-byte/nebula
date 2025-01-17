83b74ab81c440e57f6c55ef3221a805f210acdbb
share-a-byte <write2vaniramesh@gmail.com>
2025-01-17 10:22:45 -0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/images/logo.png"
+          alt="Nebula logo"
           width={180}
-          height={38}
+          height={40}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
